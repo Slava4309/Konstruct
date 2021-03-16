8467cb24a1a4d5f47bf4cb794452cbdc399d46d1
Slava <76907321+Slava4309@users.noreply.github.com>
2021-03-16 19:14:12 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* Чтобы wow эффект работал */
+new WOW().init();
+
+/* Чтобы работала кнопка */
+var modalBtn = document.querySelector ('.modal-btn');
+var modalBg = document.querySelector ('.modal-bg');
+var modalClose = document.querySelector ('.modal-close');
+
+modalBtn.addEventListener ('click', function(){
+    modalBg.classList.add('bg-active');
+});
+
+modalClose.addEventListener ('click', function(){
+    modalBg.classList.remove ('bg-active');
+});
+
+
